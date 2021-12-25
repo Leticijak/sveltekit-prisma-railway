0384667ffe9481832f79983769e805d4168e84c2
Leticijak <mnogomijeheavy@gmail.com>
2021-12-25 22:43:59 +0100

    getting html structure

---
+++ src/components/todo-item.svelte
@@ -0,0 +1,14 @@
+<div>
+	<form>
+		<input type="hidden" name="done" value="" />
+		<button aria-label="Mark Done / Not Done">Done/NotDone</button>
+	</form>
+	<form>
+		<input type="text" />
+		<button aria-label="Save Todo">Save</button>
+	</form>
+	<form>
+		<button aria-label="Delete Todo">Delete</button>
+	</form>
+</div>
+	
